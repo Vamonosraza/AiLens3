1e70b83aaa085747b152349a4cdc8f4cd0fd8996
Vamonosraza <martinezjessy393@gmail.com>
2025-04-12 17:38:16 -0700

    Initial Commit

---
+++ AiLens3/AiLens3App.swift
@@ -0,0 +1,17 @@
+//
+//  AiLens3App.swift
+//  AiLens3
+//
+//  Created by Jessy  Martinez  on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AiLens3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiLens3Tests/AiLens3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AiLens3Tests.swift
+//  AiLens3Tests
+//
+//  Created by Jessy  Martinez  on 4/12/25.
+//
+
+import Testing
+@testable import AiLens3
+
+struct AiLens3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
